2418c6e1d4e57b3e9c648cdce8d0cd0ccbec459a
alexyuisme <alexyuisme@gmail.com>
2015-02-07 21:22:07 -0800

    git commit -a

--- DataStructure/EnglishReference/CareerCup/Amazon/2DArray.java
+++ DataStructure/EnglishReference/CareerCup/Amazon/2DArray.java
@@ -27,10 +27,10 @@ Solution1:
 
 -   Solution 2
     1.  start scanning from right most end of first row and find first occurrence of 0. 
-        suppose it is some value x. then max_numer of 1 till now is col-x-1. 
-    2.  then go to next row but start from x. if you found 1 at that place then scan this 
-        row and find new x as in first step and replace max_number by new value of col-x-1. 
-    3.  repeat it till last row mean while if you get x=-1 then return the length of col. 
+        suppose it is some value y. then max_numer of 1 till now is col-y-1. 
+    2.  then go to next row but start from y. if you found 1 at that place then scan this 
+        row and find new x as in first step and replace max_number by new value of col-y-1. 
+    3.  repeat it till last row mean while if you get y=-1 then return the length of col. 
 
     Time Complexity: m*log(n), why log(n)? because we can use binary search to do row scanning.
     Space Complexity: nothing
@@ -39,13 +39,100 @@ Solution1:
 import java.util.*;
 import java.lang.*;
 
-class 2DArray {
+class TwoDArray {
+    class Pos {
+        int x;
+        int y; 
+
+        Pos(int x, int y) {
+            this.x = x;    
+            this.y = y;    
+        }
+    }
+
+    private int[][] array; //The 2D array
+
+    public TwoDArray() {
+        array = null;    
+    }
+
+    public TwoDArray(int[][] array) {
+        this.array = array;
+    }
+
+    public int whichRowHasMaxOnes_1() {
+        int col = array[0].length;
+        int i = 0;
+        while (i < col) {
+            Pos p = new Pos(0, i);
+            Pos result = verticalBinarySearch(p);
+            if (result != null) {
+                return result.x;
+            }
+            i++;
+        }
+
+        return -1;
+    }
+
+    public int whichRowHasMaxOnes_2() {
+        int row = array.length;
+        int col = array[0].length;
+        int y = col - 1;
+
+        for (int x = 0; x < row; x++) {
+            Pos p = new Pos(x, y);
+            Pop p_new = binarySearch(p);
+            if (p_new == null) { 
+                //Couldn't find  
+            }
+        }
+
+    }
 
     /*
-        search 0 that is the first  
-    */
-    private int binarySearch(boolean[][] array, int x, int y) {
+     *  Search element "0" by scanning from the position p(x, y) to the left
+     *  intput: the int array; the starting position p(x, y)
+     *  output: the position p'(x', y') that has the value "0". or null if it couldn't find the position has value "0".
+     */
+    private Pos binarySearch(Pos p) {
         
     }
+
+    /*
+        Search element "1" starting from the position p(x, y) downward
+        input:  the int array; the starting position p(x, y)
+        output: the position p'(x', y'), which has the value "1".
+    */
+    private Pos verticalBinarySearch(Pos p) {
+        if (array[p.x][p.y] == 1) {
+            return p;
+        }
+
+        int row = array.length;
+
+        while(p.x < row) {
+            if (array[p.x][p.y] == 1) {
+                return p;
+            }
+            p.x = p.x + 1;
+        }
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[][] array = new int[][]{
+            {1, 1, 1, 1},
+            {0, 0, 0, 1},
+            {0, 0, 1, 1},
+            {0, 1, 1, 1},
+        };
+    
+        TwoDArray a = new TwoDArray(array);
+        int result = a.whichRowHasMaxOnes_1();
+        System.out.println("result = " + result);
+    }
+
 
 }
