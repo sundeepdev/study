e2e5fe6b82d6f3c377d9e5f818e33268cd403ea1
alexyu <alexyuisme@gmail.com>
2015-02-12 21:41:54 -0800

    git commit -a

---
+++ DataStructure/EnglishReference/CareerCup/Amazon/MergeAndRemoveDuplicates.java
@@ -0,0 +1,29 @@
+/*
+    Merge two unsorted array and remove the duplicates from the resultant array.
+
+    Example:
+
+    Array1 = {“are”,”you”,”there”}
+    Array2={“how”,”are”,”you”}
+    output={“how”,”are”,”you”,”there”}    
+
+*/
+
+import java.util.*;
+import java.lang.*;
+
+/*
+    How can we reduce time complexity to O(n1 + n2) and space complexity to O(1)?
+
+    Solution 1: One way to do this is to use a hash table. This method takes
+
+    O(n1 + n2) time complexity and O(n1 + n2)
+*/
+
+class MergeAndRemoveDuplicates {
+    public static String[] mergeAndRemoveDuplicates(String[] first_array, String[] second_array) {
+        
+    }
+}
+
+
