7340fecced3ce77d3a5c7447508545cae4f3a51e
alexyu <alexyuisme@gmail.com>
2015-02-15 00:23:16 -0800

    git commit -a

---
+++ DataStructure/EnglishReference/CareerCup/Amazon/MaxKElemsForAMatrix.java
@@ -0,0 +1,13 @@
+/*
+For a given matrix, find the maximum product of k elements. The elements 
+can be formed from continuous 4 elements horizontally, vertically or 
+diagonally. Eg: For k= 4, the maximum product is (6*4*7*9) from the 
+last column, 
+
+1  2  0 -1  4 
+3  1  2  4  6 
+0  2  3  1  4 
+1  3  2  0  7 
+2  1  3  2  9
+
+*/
